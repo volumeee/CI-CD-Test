fba43288eaa8be92a5f1569b6933f84b8bbc96fb
Bagus Erwanto <57589007+volumeee@users.noreply.github.com>
2024-02-23 19:29:00 +0700

    tambah gradlew

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "1.5.10"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
